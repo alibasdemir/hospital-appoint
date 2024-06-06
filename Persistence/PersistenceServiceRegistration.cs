46d08d00bbd515b5973b2dbf42b817e217931368
FakirHerif <basdemir35@hotmail.com.tr>
2024-06-06 23:48:26 +0300

    add PersistenceServiceRegistration

---
+++ Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,35 @@
+﻿using Application.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services) 
+        {
+            services.AddDbContext<HospitalAppointDbContext>();
+
+            services.AddScoped<IAdminActionRepository, AdminActionRepository>();
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IDoctorAvailabilityRepository, DoctorAvailabilityRepository>();
+            services.AddScoped<IDoctorRepository, DoctorRepository>();
+            services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+            services.AddScoped<INotificationRepository, NotificationRepository>();
+            services.AddScoped<IPatientReportRepository, PatientReportRepository>();
+            services.AddScoped<IPatientRepository, PatientRepository>();
+            services.AddScoped<ISystemStatRepository, SystemStatRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}

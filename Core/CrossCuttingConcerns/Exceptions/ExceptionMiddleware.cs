6b254d72e8cf1d307258b6cd5a494d13fe17093a
FakirHerif <basdemir35@hotmail.com.tr>
2024-06-19 22:39:35 +0300

    refactor exceptionmiddleware

--- Core/CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
+++ Core/CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
@@ -1,52 +1,63 @@
-﻿using Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
-using Core.CrossCuttingConcerns.Exceptions.Types;
+﻿using System.Text.Json;
+using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Logging.Serilog;
 using Microsoft.AspNetCore.Http;
-using System.Text.Json;
 
-namespace Core.CrossCuttingConcerns.Exceptions
+namespace Core.CrossCuttingConcerns.Exceptions;
+
+public class ExceptionMiddleware
 {
-    public class ExceptionMiddleware
+    private readonly IHttpContextAccessor _contextAccessor;
+    private readonly HttpExceptionHandler _httpExceptionHandler;
+    private readonly LoggerServiceBase _loggerService;
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next, IHttpContextAccessor contextAccessor, LoggerServiceBase loggerService)
     {
-        private readonly RequestDelegate _next;
+        _next = next;
+        _contextAccessor = contextAccessor;
+        _loggerService = loggerService;
+        _httpExceptionHandler = new HttpExceptionHandler();
+    }
 
-        public ExceptionMiddleware(RequestDelegate next)
+    public async Task Invoke(HttpContext context)
+    {
+        try
         {
-            _next = next;
+            await _next(context);
         }
-
-        public async Task Invoke(HttpContext context)
+        catch (Exception exception)
         {
-            try
-            {
-                await _next(context);
-            }
-            catch(Exception exception) 
-            {
-                context.Response.ContentType = "application/json";
-                context.Response.StatusCode = StatusCodes.Status400BadRequest;
-
-                if(exception is BusinessException)
-                {
-                    ProblemDetails problemDetails = new ProblemDetails();
-                    problemDetails.Title = "Business Rule Violation";
-                    problemDetails.Detail = exception.Message;
-                    problemDetails.Type = "BusinessException";
-                    await context.Response.WriteAsync(JsonSerializer.Serialize(problemDetails));
-                }
-                else if(exception is ValidationException)
-                {
-                    ValidationException validationException = (ValidationException) exception;
-                    ValidationProblemDetails validationProblemDetails = new ValidationProblemDetails(validationException.Errors.ToList());
-
-                    await context.Response.WriteAsync(JsonSerializer.Serialize(validationProblemDetails));
-                }
-                else
-                {
-                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                    await context.Response.WriteAsync("Bilinmedik Hata");
-                }
-
-            }
+            await LogException(context, exception);
+            await HandleExceptionAsync(context.Response, exception);
         }
+    }
+
+    private Task HandleExceptionAsync(HttpResponse response, Exception exception)
+    {
+        response.ContentType = "application/json";
+        _httpExceptionHandler.Response = response;
+        return _httpExceptionHandler.HandleExceptionAsync(exception);
+    }
+
+    private Task LogException(HttpContext context, Exception exception)
+    {
+        List<LogParameter> logParameters =
+            new()
+            {
+                new LogParameter { Type = context.GetType().Name, Value = exception.ToString() }
+            };
+
+        LogDetail logDetail =
+            new()
+            {
+                MethodName = _next.Method.Name,
+                Parameters = logParameters,
+                User = _contextAccessor.HttpContext?.User.Identity?.Name ?? "?"
+            };
+
+        _loggerService.Info(JsonSerializer.Serialize(logDetail));
+        return Task.CompletedTask;
     }
 }

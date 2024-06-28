e798d302dd556c6e1fc72bda9c19f9c48544230d
FakirHerif <basdemir35@hotmail.com.tr>
2024-06-28 21:06:20 +0300

    add queries, controllers, mapping profiles for patientReport

--- Application/Features/PatientReports/Profiles/PatientReportMappingProfile.cs
+++ Application/Features/PatientReports/Profiles/PatientReportMappingProfile.cs
@@ -4,24 +4,30 @@ using Application.Features.PatientReports.Commands.Create;
 using Application.Features.PatientReports.Commands.Update;
 using AutoMapper;
 using Domain.Entities;
+using Application.Features.PatientReports.Queries.GetById;
+using Application.Features.PatientReports.Queries.GetList;
+using Core.Paging;
+using Core.Responses;
 
 namespace Application.Features.PatientReports.Profiles
 {
-    public class MappingProfile : Profile
+    public class PatientReportMappingProfile : Profile
 	{
-		public MappingProfile() 
+		public PatientReportMappingProfile() 
 		{	
 			CreateMap<PatientReport, CreatePatientReportCommand>().ReverseMap();
 			CreateMap<PatientReport, CreatePatientReportResponse>().ReverseMap();
-			
 			CreateMap<PatientReport, UpdatePatientReportCommand>().ReverseMap();
 			CreateMap<PatientReport, UpdatePatientReportResponse>().ReverseMap();
-
 			CreateMap<PatientReport, DeletePatientReportCommand>().ReverseMap();
 			CreateMap<PatientReport, DeletePatientReportResponse>().ReverseMap();
-
 			CreateMap<PatientReport, SoftDeletePatientReportCommand>().ReverseMap();
 			CreateMap<PatientReport, SoftDeletePatientReportResponse>().ReverseMap();
+			CreateMap<PatientReport, GetByIdPatientReportQuery>().ReverseMap();
+			CreateMap<PatientReport, GetByIdPatientReportResponse>().ReverseMap();
+			CreateMap<PatientReport, GetListPatientReportQuery>().ReverseMap();
+			CreateMap<PatientReport, GetListPatientReportResponse>().ReverseMap();
+			CreateMap<IPaginate<PatientReport>, GetListResponse<GetListPatientReportResponse>>().ReverseMap();
 		}
 	}
 }
---
+++ Application/Features/PatientReports/Queries/GetById/GetByIdPatientReportQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.PatientReports.Constants;
+using Application.Repositories;
+using AutoMapper;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.PatientReports.Queries.GetById
+{
+    public class GetByIdPatientReportQuery : IRequest<GetByIdPatientReportResponse>
+    {
+        public int Id { get; set; }
+
+        public class GetByIdPatientReportQueryHandler : IRequestHandler<GetByIdPatientReportQuery, GetByIdPatientReportResponse>
+        {
+            private readonly IPatientReportRepository _patientReportRepository;
+            private readonly IMapper _mapper;
+
+            public GetByIdPatientReportQueryHandler(IPatientReportRepository patientReportRepository, IMapper mapper)
+            {
+                _patientReportRepository = patientReportRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<GetByIdPatientReportResponse> Handle(GetByIdPatientReportQuery request, CancellationToken cancellationToken)
+            {
+                PatientReport? patientReport = await _patientReportRepository.GetAsync(i => i.Id  == request.Id);
+
+                if (patientReport == null || patientReport.IsDeleted == true) 
+                {
+                    throw new NotFoundException(PatientReportsMessages.PatientReportNotExists);
+                }
+
+                GetByIdPatientReportResponse response = _mapper.Map<GetByIdPatientReportResponse>(patientReport);
+                return response;
+            }
+        }
+    }
+}

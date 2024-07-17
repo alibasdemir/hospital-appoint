3ed3c89121178db46e1af33febd0483bc4bbc0c8
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-07-17 16:48:23 +0300

    Updated: AppointmentService with Create and Delete operations

--- Application/Services/AppointmentService/IAppointmentService.cs
+++ Application/Services/AppointmentService/IAppointmentService.cs
@@ -1,7 +1,11 @@
-﻿namespace Application.Services.AppointmentService
+﻿using Domain.Entities;
+
+namespace Application.Services.AppointmentService
 {
 	public interface IAppointmentService
 	{
-		Task<bool> AppointmentValidationById(int id);
-	}
+        Task<bool> AppointmentValidationById(int id);
+        Task CreateAppointments(DoctorAvailability doctorAvailability, int intervalInMinutes);
+        Task DeleteAppointment(int appointmentId);
+    }
 }

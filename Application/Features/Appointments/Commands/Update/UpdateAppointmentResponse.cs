c1867c9a4f73ad51fa1185d74af132ff01286e12
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-07-17 16:44:05 +0300

    Updated: update command for Appointment

--- Application/Features/Appointments/Commands/Update/UpdateAppointmentResponse.cs
+++ Application/Features/Appointments/Commands/Update/UpdateAppointmentResponse.cs
@@ -6,10 +6,6 @@ namespace Application.Features.Appointments.Commands.Update
     {
         public int Id { get; set; }
         public int PatientId { get; set; }
-		public int DoctorAvailabilityId { get; set; }
-		public AppointmentStatus Status { get; set; }
-		public DateTime StartTime { get; set; }
-		public DateTime EndTime { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public AppointmentStatus Status { get; set; }
     }
 }
--- Application/Features/Appointments/Commands/Update/UpdateAppointmentValidator.cs
+++ Application/Features/Appointments/Commands/Update/UpdateAppointmentValidator.cs
@@ -7,10 +7,10 @@ namespace Application.Features.Appointments.Commands.Update
         public UpdateAppointmentValidator()
         {
 			RuleFor(i => i.PatientId).NotEmpty().WithMessage("PatientId can not be empty.");
-			RuleFor(i => i.DoctorAvailabilityId).NotEmpty().WithMessage("DoctorAvailabilityId can not be empty.");
+			//RuleFor(i => i.DoctorAvailabilityId).NotEmpty().WithMessage("DoctorAvailabilityId can not be empty.");
 			RuleFor(i => i.Status).NotEmpty().WithMessage("Status can not be empty.");
-			RuleFor(i => i.StartTime).NotEmpty().WithMessage("StartTime can not be empty.");
-			RuleFor(i => i.EndTime).NotEmpty().WithMessage("EndTime can not be empty.");
+			//RuleFor(i => i.StartTime).NotEmpty().WithMessage("StartTime can not be empty.");
+			//RuleFor(i => i.EndTime).NotEmpty().WithMessage("EndTime can not be empty.");
 		}
     }
 }
